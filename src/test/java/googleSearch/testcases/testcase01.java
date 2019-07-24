d6562f8c688e426acf1de7682866815cbebfbc47
Qiaoluo He <qhe@vantage.com>
2019-07-24 17:07:44 -0400

    revise browser and add base test case

--- src/test/java/googleSearch/testcases/testcase01.java
+++ src/test/java/googleSearch/testcases/testcase01.java
@@ -5,13 +5,17 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -23,23 +27,80 @@ public class testcase01 {
 	WebDriver driver;
 	//cb.setup("chrome");
 
-	@Test
-	@Parameters("browser")// use paramenter to set browser type
-	public void clickTabOnGooleSearchResultsPage(String browser) throws Exception {
-		crossBrowserScript cb = new crossBrowserScript(driver);
-		cb.setup(browser);
+	
+	@Test(priority=0)
+	public void lauchGoogleWeb() throws Exception {
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream("C:\\Users\\qhe\\eclipse-workspace\\googleSearch\\src\\test\\java\\globalParameters.properties");
+		prop.load(fis);
+
+		if(prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\qhe\\Downloads\\chromedriver.exe");
+			driver = new ChromeDriver();
+
+		}else if (prop.getProperty("browser").equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.firefox.marionette", ".\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		else if(prop.getProperty("browser").equalsIgnoreCase("Edge")){
+			//set path to Edge.exe
+			System.setProperty("webdriver.edge.driver",".\\MicrosoftWebDriver.exe");
+			//create Edge instance
+			driver = new EdgeDriver();
+		}else{
+			//If no browser passed throw exception
+			throw new Exception("Browser is not correct");
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		driver.get(prop.getProperty("startUrl"));
+		
+		//driver.get(prop.getProperty("startUrl"));
+		
+		
+	}
+
+	@Test(priority=1)	
+	//@Parameters("browser")// use paramenter to set browser type, not work for @afterTest
+	public void clickTabOnGooleSearchResultsPage() throws Exception {
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream("C:\\Users\\qhe\\eclipse-workspace\\googleSearch\\src\\test\\java\\globalParameters.properties");
+		prop.load(fis);
+
+		if(prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\qhe\\Downloads\\chromedriver.exe");
+			driver = new ChromeDriver();
+
+		}else if (prop.getProperty("browser").equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.firefox.marionette", ".\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
+		else if(prop.getProperty("browser").equalsIgnoreCase("Edge")){
+			//set path to Edge.exe
+			System.setProperty("webdriver.edge.driver",".\\MicrosoftWebDriver.exe");
+			//create Edge instance
+			driver = new EdgeDriver();
+		}else{
+			//If no browser passed throw exception
+			throw new Exception("Browser is not correct");
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+
+		driver.get(prop.getProperty("startUrl"));
+
+		//crossBrowserScript cb = new crossBrowserScript(driver);
+		//cb.setup(browser);
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\qhe\\Downloads\\chromedriver.exe");
 		//driver = new ChromeDriver();
-		cb.driver.manage().window().maximize();
 		
-		
-		Properties prop = new Properties();
-		FileInputStream fis = new FileInputStream("C:\\Users\\qhe\\eclipse-workspace\\Test01\\src\\test\\java\\globalParameters.properties");
-		prop.load(fis);
-		cb.driver.get(prop.getProperty("startUrl"));
+		driver.manage().window().maximize();
+
+		driver.get(prop.getProperty("startUrl"));
 		
 		Thread.sleep(6000);
-		googleSearchResultsPage gsrp = new googleSearchResultsPage(cb.driver);
+		googleSearchResultsPage gsrp = new googleSearchResultsPage(driver);
 		gsrp.chooseTab("All").click();
 		gsrp.chooseTab("News").click();
 		gsrp.chooseTab("Images").click();
@@ -48,6 +109,9 @@ public class testcase01 {
 
 	}
 
+	
+	
+	// screen shot function, not sure should be placed there
 	public static void captureScreenShot(WebDriver ldriver) {
 		// Take screenshot and store as a file format
 		File src = ((TakesScreenshot) ldriver).getScreenshotAs(OutputType.FILE);
@@ -62,9 +126,10 @@ public class testcase01 {
 		}
 	}
 
-//	@AfterTest
-//	public void tearDown() {
-//		driver.close();
-//	}
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+		
+	}
 
 }
--- src/test/java/objectRepository/googleSearchResultsPage.java
+++ src/test/java/objectRepository/googleSearchResultsPage.java
@@ -9,26 +9,28 @@ import org.openqa.selenium.support.PageFactory;
 public class googleSearchResultsPage {
 	WebDriver driver;
 
-	@FindBy(xpath = "//*[@id='iSeekSearchBox']")
-	WebElement searchBox;
+	@FindBy(xpath = "//input[@title=\"Search\"]")
+	public WebElement searchBox;
 	
 	@FindBy(xpath = "//*[@id=\"iSeekSearchButton\"]")
-	WebElement searchButton;
+	public WebElement searchButton;
 	
 	@FindBy(css = "#nav > tbody")
-	WebElement pageGroup;
-	
-	public googleSearchResultsPage(WebDriver driver) {
-		this.driver=driver;
-		PageFactory.initElements(driver, this);
-	}
-	
+	public WebElement pageGroup;
+
     public WebElement chooseTab(String tabName) {
     	if(tabName=="All")
     		return this.driver.findElement(By.xpath("//*[@id=\"hdtb-msb-vis\"]/div[contains(text(),\""+tabName+"\")]"));
     	else 
     		return this.driver.findElement(By.xpath("//*[@id=\"hdtb-msb-vis\"]//a[contains(text(),\""+tabName+"\")]"));
      }
+    
+	
+	public googleSearchResultsPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
     
     
 }
